69e017ddd2f012a02fcecf3cfa58e8af4547e471
alabarjasteh <amirala2011@gmail.com>
2020-12-14 11:01:25 +0330

    Initial Commit

---
+++ SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by Amirala on 9/24/1399 AP.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
